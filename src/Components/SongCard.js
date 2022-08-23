97b940f9cc96cf50e123d38fc8d2f8956bb939bc
liz <rhodes.liz@gmail.com>
2022-08-23 18:39:25 -0400

    rendered with image,artist,title and onClick to show lyrics

--- src/Components/SongCard.js
+++ src/Components/SongCard.js
@@ -2,22 +2,28 @@
 import React, { useState } from "react"
 
 function SongCard ({song}) {
-    const [showLyrics, setShowLyrics] = useState(true)
-console.log(song)
+    const [showLyrics, setShowLyrics] = useState(false)
+
+   const handleLyrics= () => {
+        setShowLyrics(showLyrics => !showLyrics)
+    }
 
 return (
     <li>
-        <div className="image">
+        <div onClick={handleLyrics} className="image">
             <img alt="album" src={song.image}/>
         </div>
         <div className="content">
-           <div className="header">{song.title}</div>
+           <div className="header">{song.title} </div>
+           <div className="header">{song.artist}</div>
+           <div>{showLyrics ? song.lyrics : null}</div>
          </div> 
          <div className="lyrics">
         </div> 
     </li>
 )
 }
+
 
 
 
