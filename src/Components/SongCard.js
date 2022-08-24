c8181f36a892fb5357c4a7499eacc006678b4951
liz <rhodes.liz@gmail.com>
2022-08-24 14:01:09 -0400

    work in progress on css, and mouseEnter/Leave. working toggle for like/dislike

--- src/Components/SongCard.js
+++ src/Components/SongCard.js
@@ -9,28 +9,27 @@ function SongCard ({song}) {
     }
     const handleToggle = (e) => {
         setLikeToggle(likeToggle => !likeToggle)
-        console.log('hey there')
     }
 
 return (
     <div>
-        <div className="image">
-            <img onClick={handleLyrics} alt="album" src={song.image}/>
+        <br></br>
+        <br></br>
+        <div className="image" onMouseLeave={handleLyrics}>
+            <img onMouseEnter={handleLyrics} alt="album" src={song.image}/>
         </div>
-        <div className="content">
-           <div className="header">{song.title} </div>
-           <div className="header">{song.artist}</div>
-           <div>{showLyrics ? song.lyrics : null}</div>
-         </div> 
-         <div className="lyrics">
-        </div> 
         <div>
-        {showLyrics ? (
+        {likeToggle ? (
         <button onClick={handleToggle} className="likeToggle" >I dig it!</button>
         ) : (
-        <button onClick={handleToggle} className="dislikeToggle" >Not for me, NEXT!</button>
+        <button onClick={handleToggle} className="likeToggle" >Not for me, NEXT!</button>
         )}
         </div>  
+        <br></br>
+           <div className="title">{song.title} </div>
+           <div className="artist">{song.artist}</div>
+           <p className="lyrics">{showLyrics ? song.lyrics : null}</p>
+        <br></br>
     </div>
 )
 }
